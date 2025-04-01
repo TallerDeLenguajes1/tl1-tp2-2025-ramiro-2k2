b9c6965f7cdc7ee096bc3a98fbdf8d772a37f0d3
Ramiro2k2 <ramiro2k2@gmail.com>
2025-04-01 11:46:15 -0300

    inicial

---
+++ tp2_1_1.c
@@ -0,0 +1,26 @@
+#include <stdio.h>
+#include <time.h>
+#include <stdlib.h>
+
+#define N 20
+
+
+int main(){
+
+    srand(time(NULL));
+
+
+int i;
+double vt[N];
+
+for(i = 0;i<N; i++)
+{
+vt[i]=1+rand()%100;
+
+printf("%f\n",vt[i]);
+}
+
+
+
+    return 0;
+}
